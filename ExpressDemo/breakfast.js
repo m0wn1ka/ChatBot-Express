dc67fb6b018c40fa067ba3a98218baac93be2ead
radha <radha@radhas-MacBook-Pro.local>
2026-02-15 17:18:39 +0530

    jwt added

--- ExpressDemo/breakfast.js
+++ ExpressDemo/breakfast.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import jwt from "jsonwebtoken";
+
 const router = express.Router();
 
 router.use((req, res, next) => {
@@ -13,5 +15,15 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   res.send(`Id that came in params is : ${req.params.id}`);
 });
+router.post("/token", (req, res) => {
+  const {username, password } = req.body;
+
+  if (!password || !username) {
+    return res.status(400).json({ message: "password and username required" });
+  }
+  const token = jwt.sign({username}, process.env.JWT_SECRET);
+  res.json({token});
+});
+
 
 export default router;
