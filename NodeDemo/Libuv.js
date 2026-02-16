d268b2c12e2c67ab330251aea8890deecee0c9d5
radha <radha@radhas-MacBook-Pro.local>
2026-02-16 20:53:04 +0530

    node modules

--- NodeDemo/Libuv.js
+++ NodeDemo/Libuv.js
@@ -4,12 +4,16 @@ import fs from 'node:fs'
 const app = express()
 
 app.get('/read-file', (req, res) => {
+  console.log('Received request to read file')
   fs.readFile('radha.txt', 'utf8', (err, data) => {
+    console.log('File read callback executed')
     if (err) {
       return res.status(500).send('Error reading file')
     }
+    console.log('File read successfully, sending response')
     res.send(data)
   })
+  console.log('File read initiated, waiting for callback')
 })
 
 app.listen(8080, () => {
---
+++ Readme.md
@@ -0,0 +1,4 @@
+## Image of node js execution
+![alt text](image.png)
+## Lib uv
+![alt text](image-1.png)
